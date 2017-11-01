27d5cd251a8f10833ab9c3f4d2166a546e42df6f
Hassan Shamim <hassan.has.you@gmail.com>
2017-11-01 13:06:21 -0700

    use the right database

--- chinook_practice/settings.py
+++ chinook_practice/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'chinook_practice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'Chinook_Sqlite.sqlite'),
+        'NAME': os.path.join(BASE_DIR, 'chinook.db'),
     }
 }
 
