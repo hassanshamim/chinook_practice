d91b3ee6d5a417591c3fed0a92c2f25e6d53d7b6
Hassan Shamim <hassan.has.you@gmail.com>
2017-11-01 12:58:42 -0700

    configure drf, api app, and chinook db in settingspy

--- chinook_practice/settings.py
+++ chinook_practice/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'chinook_practice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'Chinook_Sqlite.sqlite'),
     }
 }
 
